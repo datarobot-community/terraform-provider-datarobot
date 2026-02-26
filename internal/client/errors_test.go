09f5953d7dcc977eb9711033df0755626d2592b2
Andrii Kislitsyn <andrii.kislitsyn@datarobot.com>
2026-02-27 01:51:45 +0200

    [APP-5509] tests

--- internal/client/errors_test.go
+++ internal/client/errors_test.go
@@ -1,32 +1,32 @@
 package client
 
 import (
-    "errors"
-    "fmt"
-    "testing"
+	"errors"
+	"fmt"
+	"testing"
 )
 
 func TestNotFoundErrorIs(t *testing.T) {
-    err := NewNotFoundError("https://staging.datarobot.com/api/v2/customModels/abc123/")
+	err := NewNotFoundError("https://staging.datarobot.com/api/v2/customModels/abc123/")
 
-    if !errors.Is(err, &NotFoundError{}) {
-        t.Error("errors.Is should return true for *NotFoundError")
-    }
-    wrapped := fmt.Errorf("wrapped: %w", err)
-    if !errors.Is(wrapped, &NotFoundError{}) {
-        t.Error("errors.Is should return true for wrapped *NotFoundError")
-    }
-    if errors.Is(err, &UnauthorizedError{}) {
-        t.Error("errors.Is should return false for wrong type")
-    }
+	if !errors.Is(err, &NotFoundError{}) {
+		t.Error("errors.Is should return true for *NotFoundError")
+	}
+	wrapped := fmt.Errorf("wrapped: %w", err)
+	if !errors.Is(wrapped, &NotFoundError{}) {
+		t.Error("errors.Is should return true for wrapped *NotFoundError")
+	}
+	if errors.Is(err, &UnauthorizedError{}) {
+		t.Error("errors.Is should return false for wrong type")
+	}
 }
 
 func TestUnauthorizedErrorIs(t *testing.T) {
-    err := NewUnauthorizedError("some resource")
-    if !errors.Is(err, &UnauthorizedError{}) {
-        t.Error("errors.Is should return true for *UnauthorizedError")
-    }
-    if errors.Is(err, &NotFoundError{}) {
-        t.Error("errors.Is should return false for wrong type")
-    }
+	err := NewUnauthorizedError("some resource")
+	if !errors.Is(err, &UnauthorizedError{}) {
+		t.Error("errors.Is should return true for *UnauthorizedError")
+	}
+	if errors.Is(err, &NotFoundError{}) {
+		t.Error("errors.Is should return false for wrong type")
+	}
 }
