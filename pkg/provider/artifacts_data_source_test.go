e1b9502974e13c754b5584447eeffee9d81f4dc6
Bruno Romano <bruno.romano@datarobot.com>
2026-06-30 11:02:21 +0200

    fix with test split

--- pkg/provider/artifacts_data_source_test.go
+++ pkg/provider/artifacts_data_source_test.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/datarobot-community/terraform-provider-datarobot/internal/client"
@@ -9,6 +10,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestAccArtifactsDataSource(t *testing.T) {
@@ -219,12 +221,26 @@ func testArtifactsDataSource(t *testing.T, name, imageURI string, isMock bool) {
 	t.Helper()
 
 	dataSourceName := "data.datarobot_artifacts.all"
+	resourceName := "datarobot_artifact.test"
 
-	steps := []resource.TestStep{
-		{
-			Config: artifactsDataSourceConfig(name, imageURI),
-			Check:  artifactsDataSourceChecks(dataSourceName, name, imageURI, isMock),
-		},
+	var steps []resource.TestStep
+	if isMock {
+		steps = []resource.TestStep{
+			{
+				Config: artifactsDataSourceConfig(name, imageURI),
+				Check:  artifactsDataSourceMockChecks(dataSourceName, name, imageURI),
+			},
+		}
+	} else {
+		steps = []resource.TestStep{
+			{
+				Config: artifactResourceOnlyConfig(name, imageURI),
+			},
+			{
+				Config: artifactsDataSourceConfig(name, imageURI),
+				Check:  artifactsDataSourceAcceptanceChecks(dataSourceName, resourceName, name, imageURI),
+			},
+		}
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -233,6 +249,14 @@ func testArtifactsDataSource(t *testing.T, name, imageURI string, isMock bool) {
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps:                    steps,
 	})
+}
+
+func artifactResourceOnlyConfig(name, imageURI string) string {
+	return fmt.Sprintf(`
+%s
+
+%s
+`, testProviderConfigBlock(), artifactResourceConfig(name, imageURI))
 }
 
 func artifactsDataSourceConfig(name, imageURI string) string {
@@ -246,7 +270,9 @@ resource "datarobot_artifact" "test" {
 %s
 }
 
-data "datarobot_artifacts" "all" {}
+data "datarobot_artifacts" "all" {
+  depends_on = [datarobot_artifact.test]
+}
 `, testProviderConfigBlock(), name, artifactTestContainerSpecBlock(imageURI))
 }
 
@@ -263,6 +289,7 @@ resource "datarobot_artifact" "test" {
 
 data "datarobot_artifacts" "locked" {
   status = %q
+  depends_on = [datarobot_artifact.test]
 }
 `, testProviderConfigBlock(), name, artifactTestContainerSpecBlock(imageURI), status)
 }
@@ -280,14 +307,15 @@ resource "datarobot_artifact" "test" {
 
 data "datarobot_artifacts" "limited" {
   limit = %d
+  depends_on = [datarobot_artifact.test]
 }
 `, testProviderConfigBlock(), name, artifactTestContainerSpecBlock(imageURI), limit)
 }
 
-func artifactsDataSourceChecks(dataSourceName, name, imageURI string, isMock bool) resource.TestCheckFunc {
+func artifactsDataSourceMockChecks(dataSourceName, name, imageURI string) resource.TestCheckFunc {
 	const containerPrefix = "artifacts.0.spec.container_groups.0.containers.0"
 
-	checks := []resource.TestCheckFunc{
+	return resource.ComposeAggregateTestCheckFunc(
 		resource.TestCheckResourceAttr(dataSourceName, "artifacts.#", "1"),
 		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.name", name),
 		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.type", "service"),
@@ -295,28 +323,95 @@ func artifactsDataSourceChecks(dataSourceName, name, imageURI string, isMock boo
 		resource.TestCheckResourceAttrSet(dataSourceName, "artifacts.0.artifact_repository_id"),
 		resource.TestCheckResourceAttrSet(dataSourceName, "artifacts.0.created_at"),
 		resource.TestCheckResourceAttrSet(dataSourceName, "artifacts.0.updated_at"),
-		resource.TestCheckResourceAttrSet(dataSourceName, "artifacts.0.status"),
+		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.status", "locked"),
+		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.version", "1"),
+		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.creator.id", "creator-id"),
+		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.tags.#", "1"),
+		resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.permissions.#", "2"),
 		resource.TestCheckResourceAttr(dataSourceName, containerPrefix+".name", "main"),
 		resource.TestCheckResourceAttr(dataSourceName, containerPrefix+".image_uri", imageURI),
 		resource.TestCheckResourceAttr(dataSourceName, containerPrefix+".description", "main container"),
 		resource.TestCheckResourceAttr(dataSourceName, containerPrefix+".primary", "true"),
 		resource.TestCheckResourceAttr(dataSourceName, containerPrefix+".port", "8080"),
-	}
+	)
+}
 
-	if isMock {
-		checks = append(checks,
-			resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.status", "locked"),
-			resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.version", "1"),
-			resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.creator.id", "creator-id"),
-			resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.tags.#", "1"),
-			resource.TestCheckResourceAttr(dataSourceName, "artifacts.0.permissions.#", "2"),
-		)
-	} else {
-		checks = append(checks,
-			resource.TestCheckResourceAttrSet(dataSourceName, "artifacts.0.creator.id"),
-			resource.TestCheckResourceAttrSet(dataSourceName, "artifacts.0.version"),
-		)
-	}
+func artifactsDataSourceAcceptanceChecks(dataSourceName, resourceName, name, imageURI string) resource.TestCheckFunc {
+	return checkArtifactListedInDataSource(dataSourceName, resourceName, name, imageURI)
+}
 
-	return resource.ComposeAggregateTestCheckFunc(checks...)
+func checkArtifactListedInDataSource(dataSourceName, resourceName, expectedName, expectedImageURI string) resource.TestCheckFunc {
+	const containerSuffix = ".spec.container_groups.0.containers.0"
+
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("resource %s not found in state", resourceName)
+		}
+
+		expectedArtifactID := rs.Primary.Attributes["artifact_id"]
+		if expectedArtifactID == "" {
+			return fmt.Errorf("artifact_id is not set on %s", resourceName)
+		}
+
+		ds, ok := s.RootModule().Resources[dataSourceName]
+		if !ok {
+			return fmt.Errorf("data source %s not found in state", dataSourceName)
+		}
+
+		countStr, ok := ds.Primary.Attributes["artifacts.#"]
+		if !ok {
+			return fmt.Errorf("artifacts.# not found on %s", dataSourceName)
+		}
+
+		count, err := strconv.Atoi(countStr)
+		if err != nil {
+			return fmt.Errorf("invalid artifacts.# value %q: %w", countStr, err)
+		}
+		if count == 0 {
+			return fmt.Errorf("expected created artifact %q in list, but artifacts.# is 0", expectedName)
+		}
+
+		for i := range count {
+			prefix := fmt.Sprintf("artifacts.%d", i)
+			attrs := ds.Primary.Attributes
+
+			if attrs[prefix+".artifact_id"] != expectedArtifactID {
+				continue
+			}
+
+			checks := map[string]string{
+				prefix + ".name":                          expectedName,
+				prefix + ".type":                          "service",
+				prefix + containerSuffix + ".name":        "main",
+				prefix + containerSuffix + ".image_uri":   expectedImageURI,
+				prefix + containerSuffix + ".description": "main container",
+				prefix + containerSuffix + ".primary":     "true",
+				prefix + containerSuffix + ".port":        "8080",
+			}
+
+			for attr, expected := range checks {
+				if attrs[attr] != expected {
+					return fmt.Errorf("expected %s = %q, got %q", attr, expected, attrs[attr])
+				}
+			}
+
+			for _, attr := range []string{
+				prefix + ".artifact_repository_id",
+				prefix + ".created_at",
+				prefix + ".updated_at",
+				prefix + ".status",
+				prefix + ".version",
+				prefix + ".creator.id",
+			} {
+				if attrs[attr] == "" {
+					return fmt.Errorf("expected %s to be set", attr)
+				}
+			}
+
+			return nil
+		}
+
+		return fmt.Errorf("artifact %q (id=%s) not found in %s list", expectedName, expectedArtifactID, dataSourceName)
+	}
 }
