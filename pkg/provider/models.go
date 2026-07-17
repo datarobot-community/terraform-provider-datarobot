730173ba813277550d46639e0c9bfbc628864788
Bruno Romano <bruno.romano@datarobot.com>
2026-07-17 11:36:39 +0200

    [RAPTOR 18571] rename rollout obj to replacement_policy

--- pkg/provider/models.go
+++ pkg/provider/models.go
@@ -1177,11 +1177,11 @@ type WorkloadResourceModel struct {
 }
 
 type WorkloadRuntimeModel struct {
-	ContainerGroups []WorkloadGroupRuntimeModel `tfsdk:"container_groups"`
-	Rollout         *WorkloadRolloutModel       `tfsdk:"rollout"`
+	ContainerGroups   []WorkloadGroupRuntimeModel     `tfsdk:"container_groups"`
+	ReplacementPolicy *WorkloadReplacementPolicyModel `tfsdk:"replacement_policy"`
 }
 
-type WorkloadRolloutModel struct {
+type WorkloadReplacementPolicyModel struct {
 	WarmupMinutes         types.Int64 `tfsdk:"warmup_minutes"`
 	KeepOldVersionMinutes types.Int64 `tfsdk:"keep_old_version_minutes"`
 }
