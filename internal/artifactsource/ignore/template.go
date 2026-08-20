5d9be362aaeeb99bcd7a736765ff630d6d91fbde
Bruno Romano <bruno.romano@datarobot.com>
2026-08-20 13:46:03 +0200

    [RAPTOR 18364] added default template support

--- internal/artifactsource/ignore/template.go
+++ internal/artifactsource/ignore/template.go
@@ -1,12 +1,21 @@
 package ignore
 
-import _ "embed"
+import (
+	"strings"
+
+	_ "embed"
+)
 
 // CLI source: cli/internal/workload/wapi/template.go
 //
-// DefaultTemplate is the .drignore content later PRs drop at source.dir on
-// first apply when generate_ignore is true. Embedded so we don't depend on
-// file lookups at runtime.
+// DefaultTemplate is the .drignore content Ensure writes at source.dir when
+// generate_ignore is true. Embedded so we don't depend on file lookups at runtime.
 //
 //go:embed drignore.tmpl
 var DefaultTemplate []byte
+
+// FromDefaultTemplate builds a Matcher from DefaultTemplate lines (plan-time
+// hashing when the file is not on disk yet).
+func FromDefaultTemplate() *Matcher {
+	return FromLines(strings.Split(string(DefaultTemplate), "\n"))
+}
